bc4eba1a136eb57ed7fe161ca9cbf08651fae6a4
Krishna Gupta <kgupta112511@gmail.com>
2025-09-05 12:40:34 +0530

    Update index.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 import dotenv from "dotenv";
-dotenv.config({
-    path: "./.env",
-});
+dotenv.config();
+
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(
